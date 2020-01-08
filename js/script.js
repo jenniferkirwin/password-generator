1f55fef962bffe873849e2e1e15ed721a667fd17
jenniferkirwin <lamontagneartist@gmail.com>
2020-01-07 22:59:27 -0500

    Began structure and style for generator

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function password() {
+    let specialChar = document.getElementById("specialChar").checked;
+    let numericChar = document.getElementById("numericChar").checked;
+    let lowercaseChar = document.getElementById("lowercaseChar").checked;
+    let uppercaseChar = document.getElementById("uppercaseChar").checked;
+    console.log(specialChar);
+    console.log(numericChar);
+    console.log(lowercaseChar);
+    console.log(uppercaseChar);
+    // var variable = document.getElementById('input_id').value;
+    // document.getElementById('alert').innerHTML = 'The user input is: ' + variable;
+  }
