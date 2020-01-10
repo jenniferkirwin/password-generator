3e139e029d59598192ab98e718a3cab12fdbed5b
jenniferkirwin <lamontagneartist@gmail.com>
2020-01-10 08:59:08 -0500

    Added "specialChar" to base password array

--- js/script.js
+++ js/script.js
@@ -1,12 +1,20 @@
 function password() {
-    let specialChar = document.getElementById("specialChar").checked;
-    let numericChar = document.getElementById("numericChar").checked;
-    let lowercaseChar = document.getElementById("lowercaseChar").checked;
-    let uppercaseChar = document.getElementById("uppercaseChar").checked;
-    console.log(specialChar);
-    console.log(numericChar);
-    console.log(lowercaseChar);
-    console.log(uppercaseChar);
+    // let specialChar = document.getElementById("specialChar").checked;
+    specialChar = " !\"#$%&\'()*+,-./:;<=>?@[\]^_\`{|}~";
+    // let numericChar = document.getElementById("numericChar").checked;
+    let numericChar = "1234567890";
+    // let lowercaseChar = document.getElementById("lowercaseChar").checked;
+    let lowercaseChar = "abcdefghijklmnopqrstuvwxyz";
+    // let uppercaseChar = document.getElementById("uppercaseChar").checked;
+    let uppercaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let additionalChar = "";
+    let basePassword = [];
     // var variable = document.getElementById('input_id').value;
     // document.getElementById('alert').innerHTML = 'The user input is: ' + variable;
+    if (document.getElementById("specialChar").checked) {
+      basePassword.push(specialChar[Math.floor(Math.random() * specialChar.length)]);
+      // additionalChar.push(specialChar);
+      console.log(basePassword);
+      console.log(additionalChar);
+    }
   }
