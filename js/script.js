9bf35d3a793f7b5890440d6f25dac8996215d66f
jenniferkirwin <lamontagneartist@gmail.com>
2020-01-11 19:42:10 -0500

    Simplified JavaScript & generated password

--- js/script.js
+++ js/script.js
@@ -1,56 +1,70 @@
 function password() {
-    // Starting variables and characters to generate password
-    let passwordLength = document.getElementById('passwordLength').value;
-    let specialChar = " !\"#$%&\'()*+,-./:;<=>?@[\]^_\`{|}~";
-    let numericChar = "1234567890";
-    let lowercaseChar = "abcdefghijklmnopqrstuvwxyz";
-    let uppercaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    let additionalChar = "";
-    let basePassword = [];
+  // Starting variables and characters to generate password
+  let passwordLength = document.getElementById('passwordLength').value;
+  let passwordPhrase = document.getElementById('passwordPhrase').value;
+  let specialChar = " !\"#$%&\'()*+,-./:;<=>?@[\]^_\`{|}~";
+  let numericChar = "1234567890";
+  let lowercaseChar = "abcdefghijklmnopqrstuvwxyz";
+  let uppercaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let additionalChar = "";
+  let basePassword = [];
 
-    if (passwordLength < 8 || passwordLength > 129) {
-      alert("Please pick a number between 8 and 128")
+  // Input Validation
+
+  if (passwordLength < 8 || passwordLength > 129) {
+    alert("Please pick a number between 8 and 128");
+    return;
+  };
+
+  // Password Generation
+
+  function passGen(charType, _idName) {
+    if (document.getElementById(_idName).checked) {
+      basePassword.push(charType[Math.floor(Math.random() * charType.length)]);
+      additionalChar = additionalChar += charType;
     };
+  };
 
-    //count how many true values, if less than 0, then throw error
-    //[true,false,true,false,true].filter(Boolean).length
-    //console.log(document.getElementById("specialChar").checked)
+  passGen(uppercaseChar, "uppercaseChar");
+  passGen(lowercaseChar, "lowercaseChar");
+  passGen(numericChar, "numericChar");
+  passGen(specialChar, "specialChar");
 
-    // Generate an array with character types selected
-    if (document.getElementById("specialChar").checked) {
-      basePassword.push(specialChar[Math.floor(Math.random() * specialChar.length)]);
-      additionalChar = additionalChar += specialChar;
+  // add to array until length = password length
+
+    for (i = basePassword.length; i < passwordLength; i++) {
+      basePassword.push(additionalChar[Math.floor(Math.random() * additionalChar.length)]);
       // console.log(basePassword);
-      // console.log(additionalChar);
-    };
+    };    
+  
+  // randomize / shuffle array
 
-    if (document.getElementById("numericChar").checked) {
-      basePassword.push(numericChar[Math.floor(Math.random() * numericChar.length)]);
-      additionalChar = additionalChar += numericChar;
-      // console.log(basePassword);
-      // console.log(additionalChar);
-    };
+  console.log(basePassword);
 
-    if (document.getElementById("lowercaseChar").checked) {
-      basePassword.push(lowercaseChar[Math.floor(Math.random() * lowercaseChar.length)]);
-      additionalChar = additionalChar += lowercaseChar;
-      // console.log(basePassword);
-      // console.log(additionalChar);
-    };
+  function shuffe(array) {
 
-    if (document.getElementById("uppercaseChar").checked) {
-      basePassword.push(uppercaseChar[Math.floor(Math.random() * uppercaseChar.length)]);
-      additionalChar = additionalChar += uppercaseChar;
-      // console.log(basePassword);
-      // console.log(additionalChar);
-    };
+	let currentIndex = array.length;
+	let temporaryValue, randomIndex;
 
-    // add to array until length = password length
+	// While there remain elements to shuffle...
+	while (0 !== currentIndex) {
+		// Pick a remaining element...
+		randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex --;
 
-    // randomize array
+		// And swap it with the current element.
+		temporaryValue = array[currentIndex];
+		array[currentIndex] = array[randomIndex];
+		array[randomIndex] = temporaryValue;
+	}
+	return array;
+};
 
-    // compile array
+  // concat array
 
-    // show password to user
+  console.log(basePassword.join(""));
 
-  }
+  // show password to user
+
+};
+
